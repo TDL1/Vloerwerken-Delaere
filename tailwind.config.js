80ad4dfa4812b161bf704e42ec54b37d0c5949b6
tristan <tristan.delaere@student.howest.be>
2024-12-18 00:00:09 +0100

    Update entire site layout, update colors schemes and add images

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,16 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        proxima: ['Proxima Nova', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+      },
     },
   },
   plugins: [
     function({ addUtilities }) {
       addUtilities({
         '.h-128': {
-          height: '32rem',
+          height: '36rem',
         },
       });
     },
