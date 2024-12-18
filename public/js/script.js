6039c12927a0ae2d34b8a9591dc8855207d649a5
tristan <tristan.delaere@student.howest.be>
2024-12-18 19:55:38 +0100

    Change colors scheme and fix video

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+function openVideo() {
+    document.getElementById('video-lightbox').classList.remove('hidden');
+  }
+
+  function closeVideo() {
+    document.getElementById('video-lightbox').classList.add('hidden');
+  }
