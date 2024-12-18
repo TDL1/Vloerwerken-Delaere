6b48cd0cf48ae7dc88cb009a04445708e31a832d
tristan <tristan.delaere@student.howest.be>
2024-12-18 20:37:44 +0100

    Add email functionality not working

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,22 @@
+
 function openVideo() {
     document.getElementById('video-lightbox').classList.remove('hidden');
   }
 
   function closeVideo() {
     document.getElementById('video-lightbox').classList.add('hidden');
+  }
+
+  function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "tristandelaere19@gmail.com",
+        Password : "Trisdel12",
+        To : 'tristandelaere19@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New Contact Form Enquiry",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
   }
