0ffacce611bfffca882b652cdda8feb7f6f2f427
tristan <tristan.delaere@student.howest.be>
2025-01-22 02:32:40 +0100

    Format code in all pages

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
-function showCustomAlert(message) {
+function showCustomAlert(message)
+{
   const alertBox = document.getElementById('customAlert');
   const alertMessage = document.getElementById('alertMessage');
   alertMessage.textContent = message;
   alertBox.classList.remove('hidden');
 }
 
-function closeCustomAlert() {
+function closeCustomAlert()
+{
   const alertBox = document.getElementById('customAlert');
   alertBox.classList.add('hidden');
 
   document.getElementById('name').value = "";
   document.getElementById('email').value = "";
   document.getElementById('phone').value = "";
-  
-  if  (document.getElementById('message'))
+
+  if (document.getElementById('message'))
   {
     document.getElementById('message').value = "";
   }
-  if (document.getElementById('location')) {
+  if (document.getElementById('location'))
+  {
     document.getElementById('location').value = "";
     document.getElementById('description').value = "";
     document.querySelectorAll('input[name="services"]').forEach(checkbox => checkbox.checked = false);
@@ -27,8 +30,10 @@ function closeCustomAlert() {
 }
 
 
-function sendContactEmail() {
-  try {
+function sendContactEmail()
+{
+  try
+  {
     document.querySelector('#submitButton').disabled = true;
 
     var templateParams = {
@@ -42,30 +47,35 @@ function sendContactEmail() {
     const templateID = "template_by9n8xi";
 
     emailjs.send(serviceID, templateID, templateParams).then(
-      (response) => {
+      (response) =>
+      {
         console.log('SUCCESS!', response.status, response.text);
         showCustomAlert("We hebben uw bericht ontvangen. Bedankt!");
       },
-      (error) => {
+      (error) =>
+      {
         console.log('FAILED...', error);
         showCustomAlert("Het versturen van uw bericht is mislukt. Probeer het later opnieuw.");
         document.querySelector('#submitButton').disabled = false;
       }
     );
-  } catch (err) {
+  } catch (err)
+  {
     console.error("Unexpected error:", err);
     showCustomAlert("Er is een onverwachte fout opgetreden. Probeer het later opnieuw.");
     document.querySelector('#submitButton').disabled = false;
   }
 }
 
-function sendOfferEmail() {
-  try {
+function sendOfferEmail()
+{
+  try
+  {
     document.querySelector('#submitButton').disabled = true;
 
     var services = Array.from(document.querySelectorAll('input[name="services"]:checked'))
-    .map(checkbox => checkbox.value)
-    .join(', ');
+      .map(checkbox => checkbox.value)
+      .join(', ');
 
     var templateParams = {
       to_name: "Vloerwerken Delaere",
@@ -81,16 +91,19 @@ function sendOfferEmail() {
     const templateID = "template_2wsidgk";
 
     emailjs.send(serviceID, templateID, templateParams).then(
-      (response) => {
+      (response) =>
+      {
         console.log('SUCCESS!', response.status, response.text);
         showCustomAlert("We hebben uw offerteaanvraag ontvangen. Bedankt!");
       },
-      (error) => {
+      (error) =>
+      {
         console.log('FAILED...', error);
         showCustomAlert("Het versturen van uw offerteaanvraag is mislukt. Probeer het later opnieuw.");
       }
     );
-  } catch (err) {
+  } catch (err)
+  {
     console.error("Unexpected error:", err);
     showCustomAlert("Er is een onverwachte fout opgetreden. Probeer het later opnieuw.");
   }
